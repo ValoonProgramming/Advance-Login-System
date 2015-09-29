dc12973153cda0035a42758215d05a74d4e490f2
ValoonProgramming <nicholasenglish36@gmail.com>
2015-09-29 18:14:33 -0400

    Update login.php

--- login.php
+++ login.php
@@ -1,1 +1,21 @@
-
+<?php
+error_reporting(E_ALL);
+require "configuration/index.php";
+if ($login === true) {
+  $loginclosed = false;
+} elseif {
+  $loginclosed = true;
+} else {
+}
+session_start();
+if ($_SESSION["logged"] === true) {
+  header("Location: ".$url."/dashboard");
+  exit;
+} else {
+}
+if (isset($_POST["account-login"])) {
+  $account_email = $_POST['account-email'];
+  $account_password = $_POST['account-password'];
+  
+}
+?>
